1660a5ccf25311a576fee9e4311836ccaed44ca6
Kesha C Lake <keshalakemusic@gmail.com>
2019-04-11 17:06:09 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,7 @@
 require 'pry'
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
+hash = {:blake => 500, :ashley => 2, :adam => 1}
 
 def key_for_min_value(name_hash)
   name_hash.collect do |item, quantity|
@@ -9,3 +10,5 @@ def key_for_min_value(name_hash)
   end
   puts name_hash.first
 end
+
+key_for_min_value(hash)
