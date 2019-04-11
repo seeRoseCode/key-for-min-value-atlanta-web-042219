e7221005f601eea4d9ccb22e17a18a941af5f9e6
Kesha C Lake <keshalakemusic@gmail.com>
2019-04-11 17:29:43 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,7 +2,7 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
 # old_hash = {blake: 500, ashley: 2, adam: 1}
-
+require 'pry'
 def key_for_min_value(name_hash)
   new_array = []
   name_hash.each do |item, quantity|
@@ -10,6 +10,8 @@ def key_for_min_value(name_hash)
     new_array << quantity
   end
   puts new_array
+  binding.pry
+
 end
 
 # key_for_min_value(old_hash)
