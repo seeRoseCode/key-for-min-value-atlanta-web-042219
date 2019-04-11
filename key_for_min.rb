e8a9bc5c8bbac16e98037de814a015d17e9efb3f
Kesha C Lake <keshalakemusic@gmail.com>
2019-04-11 17:13:14 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,9 +5,12 @@ hash = {:blake => 500, :ashley => 2, :adam => 1}
 
 def key_for_min_value(name_hash)
   name_hash.collect do |item, quantity|
-    item <=> quantity
+    new_array = []
+    puts quantity
+    new_array << quantity
+    binding.pry
   end
-  puts name_hash.first
+
 end
 
 key_for_min_value(hash)
