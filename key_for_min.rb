e489a619b48d4b8ebc0ea06cec4abfb4935a0df0
Kesha C Lake <keshalakemusic@gmail.com>
2019-04-11 17:02:54 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,7 +2,7 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  name_hash.each do |item, quantity|
+  name_hash.collect do |item, quantity|
     item <=> quantity
   end
   puts name_hash.first
